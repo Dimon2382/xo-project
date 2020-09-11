d2d3c632f486f3d17ca15d90729140e5d1cbe00d
Dima Don <dimadch@ukr.net>
2020-09-11 11:22:16 +0300

    check point

--- src/tect/dima/xo/model/Field.java
+++ src/tect/dima/xo/model/Field.java
@@ -1,9 +1,17 @@
 package tect.dima.xo.model;
 
+import tect.dima.xo.model.exceptions.AlreadyOccupaiedException;
+import tect.dima.xo.model.exceptions.InvalidPointException;
+
+
 import java.awt.*;
 
 public class Field {
     private static final int FIELD_SIZE = 3;
+
+    private static int MIN_COORDINATE = 0;
+
+    private static int MAX_COORDINATE = FIELD_SIZE;
 
     private Figure [][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -11,11 +19,27 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(Point point){
+    public Figure getFigure(Point point) throws InvalidPointException {
+        if(!checkPoint(point)){
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(Point point, Figure figure){
+    public void setFigure(Point point, Figure figure) throws InvalidPointException, AlreadyOccupaiedException {
+        if(!checkPoint(point)){
+            throw new InvalidPointException();
+        }
+        if(field[point.x][point.y] != null){
+            throw new AlreadyOccupaiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+// check valid coordinates
+    private boolean checkPoint(Point point){
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+    private boolean checkCoordinate(int coordinate){
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
