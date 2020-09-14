0b12763d4264f1d4daf0ec27899e1fac209beab1
Dima Don <dimadch@ukr.net>
2020-09-14 15:33:14 +0300

    refactoring field to be size flexible

--- src/models/Field.java
+++ src/models/Field.java
@@ -7,16 +7,20 @@ import models.exceptions.AlreadyOccupaiedException;
 import java.awt.*;
 
 public class Field {
-    private static final int FIELD_SIZE = 3;
 
     private static int MIN_COORDINATE = 0;
 
-    private static int MAX_COORDINATE = FIELD_SIZE;
+    private Figure [][] field;
 
-    private Figure [][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+    private int fieldSize;
+
+    public Field(int fieldSize) {
+        this.fieldSize = fieldSize;
+        field = new Figure[fieldSize][fieldSize];
+    }
 
     public int getSize(){
-        return FIELD_SIZE;
+        return fieldSize;
     }
 
     public Figure getFigure(Point point) throws InvalidPointException {
@@ -35,9 +39,9 @@ public class Field {
     }
 // check valid coordinates
     private boolean checkPoint(Point point){
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+        return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field[point.x].length);
     }
-    private boolean checkCoordinate(int coordinate){
-        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    private boolean checkCoordinate(int coordinate, int maxCoordinate){
+        return coordinate >= MIN_COORDINATE && coordinate < field.length;
     }
 }
